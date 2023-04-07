31e6c92041c2d67399bc53b8725cc58c8110aab4
Jefferson Santos <31138540+Jefferson1832@users.noreply.github.com>
2023-04-07 12:31:40 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function verificar(){
     nomePessoa = document.getElementById('name').value;
     convidados = ['Alexia', 'Fabiano', 'Simone', 'Pamela', 'Gessica']
-    if(nomePessoa.includes(convidados)){
+    if(convidados.includes(nomePessoa)){
         document.getElementById('Permission').innerText = 'You are wished!'
     } else {
         document.getElementById('Permission').innerText = 'You are not wished'
